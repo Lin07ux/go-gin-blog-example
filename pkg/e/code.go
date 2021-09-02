f11514dd04bd2d54f1d2d64c906e42c583a37f14
林胜利 <linshengl@weipaitang.com>
2021-09-02 17:50:04 +0800

    feat: 添加基础错误常量和工具类

---
+++ pkg/e/code.go
@@ -0,0 +1,16 @@
+package e
+
+const (
+	Success       = 200
+	InvalidParams = 400
+	Error         = 500
+
+	ErrorExistTag        = 10001
+	ErrorNotExistTag     = 10002
+	ErrorNotExistArticle = 10003
+
+	ErrorAuthCheckTokenFail    = 20001
+	ErrorAuthCheckTokenTimeout = 20002
+	ErrorAuthToken             = 2003
+	ErrorAuth                  = 2004
+)
