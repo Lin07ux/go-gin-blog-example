a062efb5db5131c6ebb6d20f666fb209042fb58e
林胜利 <linshengl@weipaitang.com>
2021-09-03 10:43:20 +0800

    feat: 前置 gin 模式的设置，避免正式模式下依旧输出 debug 提示

--- routers/router.go
+++ routers/router.go
@@ -7,12 +7,12 @@ import (
 )
 
 func InitRouter() *gin.Engine {
+	gin.SetMode(setting.RunMode)
+
 	r := gin.New()
 
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
-
-	gin.SetMode(setting.RunMode)
 
 	apiV1 := r.Group("/api/v1")
 	{
