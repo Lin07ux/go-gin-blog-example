b20f69ea4431f6fbdb392f16ec02718d6632efcd
林胜利 <linshengl@weipaitang.com>
2021-09-03 10:34:49 +0800

    feat: 完成文章标签的更新和删除功能

--- models/tag.go
+++ models/tag.go
@@ -45,16 +45,22 @@ func GetTagTotal(maps interface{}) (count int) {
 	return
 }
 
-// 判断标签是否存在
+// 判断标签名称是否存在
 func ExistTagByName(name string) bool {
 	var tag Tag
 
 	db.Select("id").Where("name = ?", name).First(&tag)
-	if tag.ID > 0 {
-		return true
-	}
 
-	return false
+	return tag.ID > 0
+}
+
+// 判断标签是否存在
+func ExistTagById(id int) bool {
+	var tag Tag
+
+	db.Select("id").Where("id = ?", id).First(&tag)
+	
+	return tag.ID > 0
 }
 
 // 新增标签
@@ -64,6 +70,20 @@ func AddTag(name string, state int, createdBy string) bool {
 		State: state,
 		CreatedBy: createdBy,
 	})
+
+	return true
+}
+
+// 编辑标签
+func EditTag(id int, data interface{}) bool {
+	db.Model(&Tag{}).Where("id = ?", id).Update(data)
+
+	return true
+}
+
+// 删除标签
+func DeleteTag(id int) bool {
+	db.Where("id = ?", id).Delete(&Tag{})
 
 	return true
 }
--- routers/v1/tag.go
+++ routers/v1/tag.go
@@ -44,7 +44,7 @@ func AddTag(c *gin.Context) {
 	code := e.InvalidParams
 	msg := ""
 
-	result, message := validateTagData(name, state, createdBy)
+	result, message := validateCreateTagData(name, state, createdBy)
 	if result {
 		if ! models.ExistTagByName(name) {
 			code = e.Success
@@ -66,21 +66,99 @@ func AddTag(c *gin.Context) {
 
 // 修改文章标签
 func EditTag(c *gin.Context) {
-	//
+	id := com.StrTo(c.Param("id")).MustInt()
+	state := com.StrTo(c.DefaultPostForm("state", "-1")).MustInt()
+	name := c.PostForm("name")
+	modifiedBy := c.PostForm("modified_by")
+
+	code := e.InvalidParams
+	msg := ""
+
+	result, message := validateUpdateTagData(id, state, name, modifiedBy)
+	if result {
+		if models.ExistTagById(id) {
+			data := make(map[string]interface{})
+			data["modified_by"] = modifiedBy
+			if name != "" {
+				data["name"] = name
+			}
+			if state >= 0 {
+				data["state"] = state
+			}
+			models.EditTag(id, data)
+			code = e.Success
+		} else {
+			code = e.ErrorNotExistTag
+		}
+		msg = e.GetMsg(code)
+	} else {
+		msg = message
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg": msg,
+		"data": make(map[string]string),
+	})
 }
 
 // 删除文章标签
 func DeleteTag(c *gin.Context) {
-	//
+	id := com.StrTo(c.Param("id")).MustInt()
+
+	valid := validation.Validation{}
+	valid.Min(id, 1, "id").Message("标签 ID 不存在")
+
+	code := e.Success
+	msg := ""
+
+	if ! valid.HasErrors() {
+		if models.ExistTagById(id) {
+			models.DeleteTag(id)
+		} else {
+			code = e.ErrorNotExistTag
+		}
+		msg = e.GetMsg(code)
+	} else {
+		code = e.InvalidParams
+		msg = valid.Errors[0].Message
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg": msg,
+		"data": make(map[string]string),
+	})
 }
 
-func validateTagData(name string, state int, createdBy string) (result bool, message string) {
+// 校验创建文章标签的数据
+func validateCreateTagData(name string, state int, createdBy string) (bool, string) {
 	valid := validation.Validation{}
 	valid.Required(name, "name").Message("标签名称不能为空")
 	valid.MaxSize(name, 100, "name").Message("标签名称最长为 100 个字符")
 	valid.Required(createdBy, "created_by").Message("创建人不能为空")
 	valid.MaxSize(createdBy, 100, "created_by").Message("创建人最长为 100 个字符")
 	valid.Range(state, 0, 1, "state").Message("状态只允许为 0、1")
+
+	if valid.HasErrors() {
+		return false, valid.Errors[0].Message
+	}
+
+	return true, ""
+}
+
+// 校验更新文章标签的数据
+func validateUpdateTagData(id int, state int, name string, modifiedBy string) (bool, string) {
+	valid := validation.Validation{}
+	valid.Required(id, "id").Message("标签 ID 不能为空")
+	valid.Min(id, 1, "id").Message("标签 ID 不存在")
+	valid.MaxSize(name, 100, "name").Message("标签名称最长为 100 个字符")
+	valid.Required(modifiedBy, "modified_by").Message("修改人不能为空")
+	valid.MaxSize(modifiedBy, 100, "modified_by").Message("修改人最长为 100 个字符")
+
+	if state >= 0 {
+		valid.Range(state, 0, 1, "state").Message("标签状态只能为 0、1")
+	}
 
 	if valid.HasErrors() {
 		return false, valid.Errors[0].Message
